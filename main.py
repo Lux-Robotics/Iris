f82f99e7786716a5536c2388448d63c95448ae0e
Benson Zhou <bensonzhou@yahoo.com>
2023-08-28 15:45:24 -0700

    fixed config issue

--- main.py
+++ main.py
@@ -13,31 +13,28 @@ prev_frame_time = 0
 fps = [0 for x in range(10)]
 
 
-try:
-    while True:
-        new_frame_time = time.time()
-        ret, frame = camera.read()
-        # print(frame.shape)
-        # image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        image = frame
-        ids, corners = find_corners(image)
-        im = draw_detections(frame, zip(ids, corners))
-        a = solvepnp_singletag(zip(ids, corners))
-        # print(a)
-        # if 3 in a:
-        #     print("y:", a[3][1][0][0].round(2), "z:", a[3][1][1][0].round(2), "x:", a[3][1][2][0].round(2))
+while True:
+    new_frame_time = time.time()
+    ret, frame = camera.read()
+    # print(frame.shape)
+    # image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    image = frame
+    ids, corners = find_corners(image)
+    im = draw_detections(frame, zip(ids, corners))
+    a = solvepnp_singletag(zip(ids, corners))
+    # print(a)
+    # if 3 in a:
+    #     print("y:", a[3][1][0][0].round(2), "z:", a[3][1][1][0].round(2), "x:", a[3][1][2][0].round(2))
 
-        cv2.putText(im, "FPS: " + str(10/(new_frame_time - fps[-10])), (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
-        print(str(10/(new_frame_time - fps[-10])))
-        cv2.imshow("image", im)
+    cv2.putText(im, "FPS: " + str(10/(new_frame_time - fps[-10])), (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
+    print(str(10/(new_frame_time - fps[-10])))
+    cv2.imshow("image", im)
 
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
-            break
-        fps.append(new_frame_time)
-        fps = fps[-10:]
-except:
-    pass
+    k = cv2.waitKey(1) & 0xff
+    if k == 27:
+        break
+    fps.append(new_frame_time)
+    fps = fps[-10:]
 
 camera.release()
 cv2.destroyAllWindows()
--- util/constants.py
+++ util/constants.py
@@ -8,8 +8,8 @@ f.close()
 
 c = open(settings["calibration"], 'r')
 calibration = json.load(c)
-camera_matrix = calibration["cameraMatrix"]
-dist_coeffs = calibration["distCoeffs"]
+camera_matrix = np.array(calibration["cameraMatrix"])
+dist_coeffs = np.array(calibration["distCoeffs"])
 resolution = tuple(calibration["resolution"])
 c.close()
 
