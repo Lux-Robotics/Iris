b179f648a03892267c8c3d4a55df9d176b7b8e8b
Ashray._.g <ashray.gupta@gmail.com>
2023-08-30 09:32:58 -0700

    added decision margin

--- detectors/apriltag_detector.py
+++ detectors/apriltag_detector.py
@@ -9,6 +9,11 @@ detector = apriltag.Detector(constants.detector_options)
 def find_corners(image):
     # Detect AprilTags in the image
     detections = detector.detect(image)
+    detections_filtered = []
+    for detection in detections:
+        margin = getattr(detection, 'decision_margin')
+        if margin > 35:
+            detections_filtered.append(detection)
     if len(detections) == 0:
         return []
-    return [TagObservation(detection.tag_id, detection.corners.reshape((1, 4, 2)).astype(np.float32)) for detection in detections]
+    return [TagObservation(detection.tag_id, detection.corners.reshape((1, 4, 2)).astype(np.float32)) for detection in detections_filtered]
