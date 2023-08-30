d1d980fc758324115dd2d3a9b68e0e64ea138274
Benson Zhou <bensonzhou@yahoo.com>
2023-08-29 19:57:25 -0700

    cleanup main.py

--- main.py
+++ main.py
@@ -11,26 +11,33 @@ camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
 prev_frame_time = 0
 fps = [0 for x in range(10)]
 
-
 while True:
     new_frame_time = time.time()
     ret, frame = camera.read()
 
-    detections = find_corners(frame)
+    if frame is None:
+        print("video input not detected")
+        time.sleep(0.02)
+        continue
 
+    detections = find_corners(frame)
 
     # Solve for pose
     poses = solvepnp_singletag(detections)
+
     if constants.preview:
         im = draw_detections(frame, detections)
 
         if constants.preview_fps:
-            cv2.putText(im, "FPS: " + str(10/(new_frame_time - fps[-10])), (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
-            cv2.putText(im, "Frame: " + str(len(fps) - 9), (7, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(im, "FPS: " + str(10 / (new_frame_time - fps[-10])), (7, 40), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (100, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(im, "Frame: " + str(len(fps) - 9), (7, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2,
+                        cv2.LINE_AA)
 
         if constants.preview_pose:
             try:
-                pose_text = "y:" + str(poses[3][1][0][0].round(2)) + "z:" + str(poses[3][1][1][0].round(2)) + "x:" + str(poses[3][1][2][0].round(2))
+                pose_text = " y: " + str(poses[3][1][0][0].round(2)) + " z: " + str(
+                    poses[3][1][1][0].round(2)) + " x: " + str(poses[3][1][2][0].round(2))
                 cv2.putText(im, pose_text, (7, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
             except:
                 pass
@@ -41,8 +48,11 @@ while True:
             break
         if constants.preview_frame:
             cv2.waitKey(0)
-    fps.append(new_frame_time)
 
+    else:
+        print("FPS:", 10 / (new_frame_time - fps[-10]))
+
+    fps.append(new_frame_time)
 
 camera.release()
 cv2.destroyAllWindows()
