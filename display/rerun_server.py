12aa637f5820cbf6e227b3673d0f68a338bc65ad
Benson Zhou <bensonzhou@yahoo.com>
2023-09-24 21:58:16 -0700

    add rerun logging

--- display/rerun_server.py
+++ display/rerun_server.py
@@ -1,26 +1,16 @@
-import math
-import time
-
-import cv2.gapi
 import rerun as rr
 
 import util.config as config
 import display.pipeline
 
-rr.init("PeninsulaPerception", spawn=True)
+rr.init("PeninsulaPerception", spawn=False)
 
+rr.save("log.rrd")
 
 def start():
     while True:
-        try:
-            frame = display.pipeline.process()
-            scale = math.ceil(max(frame.shape[1] / config.stream_res[0], frame.shape[0] / config.stream_res[1]))
-            frame = cv2.resize(frame, dsize=(int(frame.shape[1] / scale), int(frame.shape[0] / scale)))
-            if frame is None:
-                continue
-            else:
-                rr.log_image("test_img", frame, jpeg_quality=30)
-            time.sleep(0.02)
-        except:
-            time.sleep(0.02)
+        frame = display.pipeline.process()
+        if frame is None:
             continue
+        else:
+            rr.log_image("stream", frame, jpeg_quality=25)
