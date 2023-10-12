fix rerun rotation

util/vision_types.py
@@ -18,17 +18,22 @@ class Pose:
     error: float
 
     def get_object_pose(self):
+        pose = self.get_wpilib()
+        pose = pose.transformBy(Transform3d(Translation3d(), Rotation3d(Quaternion(-0.5, 0.5, -0.5, 0.5))))
+        world_pose = Transform3d(pose.translation(), pose.rotation())
+        target_pose = world_pose.inverse()
+        Rotation3d
+        return Pose3d(target_pose.translation(), target_pose.rotation())
+
+    def get_wpilib(self):
         # shift coordinate system
         tvec = np.array([self.tvec[2], -self.tvec[0], -self.tvec[1]])
 
-        return Pose3d(
+        pose = Pose3d(
             Translation3d(tvec[0][0], tvec[1][0], tvec[2][0]),
             Rotation3d(np.array([self.rvec[2][0], -self.rvec[0][0], -self.rvec[1][0]]),
                        math.sqrt(
                            math.pow(self.rvec[0][0], 2) + math.pow(self.rvec[1][0], 2) + math.pow(self.rvec[2][0], 2))))
-
-    def get_wpilib(self):
-        pose = self.get_object_pose()
 
         # solvepnp returns pose to object, invert to get camera pose
         target_pose = Transform3d(pose.translation(), pose.rotation())