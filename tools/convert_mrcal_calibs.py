afd3802031a30416951faf528f11805e4f320eda
Benson Zhou <bensonzhou@yahoo.com>
2023-10-18 18:02:16 -0700

    add mrcal conversion tool

---
+++ tools/convert_mrcal_calibs.py
@@ -0,0 +1,17 @@
+# Tool to convert mrcal calibration to a normal .json
+
+import argparse
+import sys
+
+import mrcal
+
+parser = argparse.ArgumentParser("mrcal_converter")
+
+parser.add_argument("filepath", help="Path to your .cameramodel file", type=str)
+args = parser.parse_args()
+
+try:
+    model = mrcal.cameramodelcameramodel(args.filename)
+except:
+    print("Error loading calibration file:", args.filename)
+    sys.exit()
