b175ab33fd4a61a52c0179b7f0dfdd0c5f9ac1b5
Benson Zhou <bensonzhou@yahoo.com>
2023-10-18 19:23:37 -0700

    fix duplicate

--- tools/convert_mrcal_calibs.py
+++ tools/convert_mrcal_calibs.py
@@ -11,7 +11,7 @@ parser.add_argument("filepath", help="Path to your .cameramodel file", type=str)
 args = parser.parse_args()
 
 try:
-    model = mrcal.cameramodelcameramodel(args.filename)
+    model = mrcal.cameramodel(args.filename)
 except:
     print("Error loading calibration file:", args.filename)
     sys.exit()
