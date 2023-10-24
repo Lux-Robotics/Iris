b8099549604fbcec5c8e850754578d5133269836
Benson Zhou <bensonzhou@yahoo.com>
2023-10-23 21:03:56 -0700

    another quick fix

--- tools/convert_mrcal_calibs.py
+++ tools/convert_mrcal_calibs.py
@@ -12,7 +12,7 @@ parser.add_argument("filepath", help="Path to your .cameramodel file", type=str)
 args = parser.parse_args()
 
 try:
-    model = mrcal.cameramodel(args.filename)
+    model = mrcal.cameramodel(args.filepath)
 except:
     print("Error loading calibration file:", args.filepath)
     sys.exit()
@@ -21,7 +21,7 @@ id_num = input("Input camera identification number (ex. 002-1): ")
 
 model_type, intrinsics_mrcal = model.intrinsics()
 
-x_res, y_res = model.imagersize()
+x_res, y_res = model.imagersize().tolist()
 
 intrinsic_matrix_opencv = [
     [intrinsics_mrcal[0], 0, intrinsics_mrcal[2]],
@@ -34,6 +34,6 @@ distortions_opencv = intrinsics_mrcal[4:].tolist()
 calibration_out = {"resolution": [x_res, y_res], "cameraMatrix": intrinsic_matrix_opencv,
                    "distCoeffs": distortions_opencv}
 
-out = open("calibration/" + id_num + "_" + x_res + "x" + y_res + ".json", "w")
+out = open("calibration/" + id_num + "_" + str(x_res) + "x" + str(y_res) + ".json", "w")
 out.write(json.dumps(calibration_out, indent=2))
 out.close()
