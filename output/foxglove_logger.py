84caeef7f2dd18377126b8dbd275f765bc9166e6
Benson Zhou <bensonzhou@yahoo.com>
2023-11-05 23:11:46 -0800

    add foxglove ws

---
+++ output/foxglove_logger.py
@@ -0,0 +1,51 @@
+# This example writes a single point cloud message.
+import time
+import os
+import datetime
+
+from mcap_protobuf.writer import Writer
+
+import output.pipeline
+import cv2
+import util.config as config
+
+from output.foxglove_image import write_frame
+from output.foxglove_pose import write_pose, setup_field
+
+
+def main():
+    # Create logs director if doesn't exist
+    if not os.path.exists("logs/"):
+        os.makedirs("logs/")
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    with open("logs/log-" + timestamp + ".mcap", "wb") as f, Writer(f) as writer:
+        start = time.time_ns()
+        setup_field(writer, start)
+        while True:
+            now = time.time_ns()
+            frame, points, ids = output.pipeline.process()
+            if frame is None:
+                continue
+            else:
+                # Encode the frame in JPEG format
+                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), config.stream_quality]
+                ret, buffer = cv2.imencode('.jpg', frame, encode_param)
+                if not ret:
+                    continue
+
+            # Convert the frame to bytes
+            data = buffer.tobytes()
+            write_frame(writer, now, data, points, ids)
+            if len(config.poses) > 0:
+                write_pose(writer, now, config.poses[0], "camera")
+            if len(config.poses) > 1:
+                write_pose(writer, now, config.poses[1], "ambiguity")
+
+
+def start():
+    main()
+
+
+if __name__ == "__main__":
+    main()
--- output/foxglove_server.py
+++ output/foxglove_server.py
@@ -1,29 +1,121 @@
-# This example writes a single point cloud message.
 import time
 import os
-import datetime
+from base64 import b64encode
 
-from mcap_protobuf.writer import Writer
-
-import output.pipeline
 import cv2
-import util.config as config
 
+from typing import Set, Type
+
+from foxglove_schemas_protobuf.CameraCalibration_pb2 import CameraCalibration
+from foxglove_schemas_protobuf.ImageAnnotations_pb2 import ImageAnnotations
+from foxglove_schemas_protobuf.CompressedImage_pb2 import CompressedImage
+from foxglove_schemas_protobuf.FrameTransform_pb2 import FrameTransform
+from foxglove_schemas_protobuf.SceneUpdate_pb2 import SceneUpdate
+from output.float_message_pb2 import FloatMessage
+from output.foxglove_pose import write_pose
 from output.foxglove_image import write_frame
-from output.foxglove_pose import write_pose, setup_field
+import util.config as config
+import output.pipeline
+
+from google.protobuf.descriptor_pb2 import FileDescriptorSet
+from google.protobuf.descriptor import FileDescriptor
+import google.protobuf.message
+
+from foxglove_websocket import run_cancellable
+from foxglove_websocket.server import FoxgloveServer, FoxgloveServerListener
 
 
-def main():
-    # Create logs director if doesn't exist
-    if not os.path.exists("logs/"):
-        os.makedirs("logs/")
+def build_file_descriptor_set(
+        message_class: Type[google.protobuf.message.Message],
+) -> FileDescriptorSet:
+    """
+    Build a FileDescriptorSet representing the message class and its dependencies.
+    """
+    file_descriptor_set = FileDescriptorSet()
+    seen_dependencies: Set[str] = set()
 
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    with open("logs/log-" + timestamp + ".mcap", "wb") as f, Writer(f) as writer:
-        start = time.time_ns()
-        setup_field(writer, start)
+    def append_file_descriptor(file_descriptor: FileDescriptor):
+        for dep in file_descriptor.dependencies:
+            if dep.name not in seen_dependencies:
+                seen_dependencies.add(dep.name)
+                append_file_descriptor(dep)
+        file_descriptor.CopyToProto(file_descriptor_set.file.add())  # type: ignore
+
+    append_file_descriptor(message_class.DESCRIPTOR.file)
+    return file_descriptor_set
+
+
+async def main():
+    async with FoxgloveServer("0.0.0.0", 8765, "example server") as server:
+        ambiguity_pose_pub = await server.add_channel(
+            {
+                "topic": "/ambiguity/pose",
+                "encoding": "protobuf",
+                "schemaName": FrameTransform.DESCRIPTOR.full_name,
+                "schema": b64encode(
+                    build_file_descriptor_set(FrameTransform).SerializeToString()
+                ).decode("ascii"),
+                "schemaEncoding": "protobuf",
+            }
+        )
+        pose_pub = await server.add_channel(
+            {
+                "topic": "/camera/pose",
+                "encoding": "protobuf",
+                "schemaName": FrameTransform.DESCRIPTOR.full_name,
+                "schema": b64encode(
+                    build_file_descriptor_set(FrameTransform).SerializeToString()
+                ).decode("ascii"),
+                "schemaEncoding": "protobuf",
+            }
+        )
+        annotations_pub = await server.add_channel(
+            {
+                "topic": "/camera/annotations",
+                "encoding": "protobuf",
+                "schemaName": ImageAnnotations.DESCRIPTOR.full_name,
+                "schema": b64encode(
+                    build_file_descriptor_set(ImageAnnotations).SerializeToString()
+                ).decode("ascii"),
+                "schemaEncoding": "protobuf",
+            }
+        )
+        calibration_pub = await server.add_channel(
+            {
+                "topic": "/camera/calibration",
+                "encoding": "protobuf",
+                "schemaName": CameraCalibration.DESCRIPTOR.full_name,
+                "schema": b64encode(
+                    build_file_descriptor_set(CameraCalibration).SerializeToString()
+                ).decode("ascii"),
+                "schemaEncoding": "protobuf",
+            }
+        )
+        fps_pub = await server.add_channel(
+            {
+                "topic": "/camera/fps",
+                "encoding": "protobuf",
+                "schemaName": FloatMessage.DESCRIPTOR.full_name,
+                "schema": b64encode(
+                    build_file_descriptor_set(FloatMessage).SerializeToString()
+                ).decode("ascii"),
+                "schemaEncoding": "protobuf",
+            }
+        )
+        image_pub = await server.add_channel(
+            {
+                "topic": "/camera/image",
+                "encoding": "protobuf",
+                "schemaName": CompressedImage.DESCRIPTOR.full_name,
+                "schema": b64encode(
+                    build_file_descriptor_set(CompressedImage).SerializeToString()
+                ).decode("ascii"),
+                "schemaEncoding": "protobuf",
+            }
+        )
         while True:
             now = time.time_ns()
+
             frame, points, ids = output.pipeline.process()
             if frame is None:
                 continue
@@ -36,16 +128,26 @@ def main():
 
             # Convert the frame to bytes
             data = buffer.tobytes()
-            write_frame(writer, now, data, points, ids)
+            img, cal, ann, fps = write_frame(now, data, points, ids)
             if len(config.poses) > 0:
-                write_pose(writer, now, config.poses[0], "camera")
+                pose = write_pose(now, config.poses[0], "camera")
             if len(config.poses) > 1:
-                write_pose(writer, now, config.poses[1], "ambiguity")
+                ambiguity = write_pose(now, config.poses[1], "ambiguity")
+            else:
+                ambiguity = None
+
+            await server.send_message(image_pub, now, img.SerializeToString())
+            await server.send_message(calibration_pub, now, cal.SerializeToString())
+            await server.send_message(annotations_pub, now, ann.SerializeToString())
+            await server.send_message(fps_pub, now, fps.SerializeToString())
+            await server.send_message(pose_pub, now, pose.SerializeToString())
+            if ambiguity is not None:
+                await server.send_message(ambiguity_pose_pub, now, ambiguity.SerializeToString())
 
 
 def start():
-    main()
+    run_cancellable(main())
 
 
 if __name__ == "__main__":
-    main()
+    run_cancellable(main())
