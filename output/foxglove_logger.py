23caa4a8405b57331a32b19327c20a9a9f43dbfe
Benson Zhou <bensonzhou@yahoo.com>
2023-11-08 22:50:18 -0800

    use python logging module instead of print

--- output/foxglove_logger.py
+++ output/foxglove_logger.py
@@ -1,7 +1,7 @@
-# This example writes a single point cloud message.
 import time
 import os
 import datetime
+import logging
 
 from mcap_protobuf.writer import Writer
 
@@ -11,6 +11,38 @@ import util.config as config
 
 from output.foxglove_image import write_frame
 from output.foxglove_pose import write_pose, setup_field
+from output.foxglove_utils import timestamp
+from foxglove_schemas_protobuf.Log_pb2 import Log
+
+
+class FoxgloveLoggingHandler(logging.Handler):
+    def __init__(self, writer: Writer):
+        super().__init__()
+        self.writer = writer
+
+    @staticmethod
+    def record_to_log(record: logging.LogRecord):
+        return Log(
+            timestamp=timestamp(int(record.created * 1e9)),
+            level=record.levelname,
+            message=record.getMessage()
+        )
+
+    def emit(self, record):
+        try:
+            now = time.time_ns()
+            log_entry = self.record_to_log(record)
+
+            # Send the log entry to Foxglove
+            self.writer.write_message(
+                topic="/log",
+                log_time=now,
+                message=log_entry,
+                publish_time=now
+            )
+        except Exception as e:
+            # Handle any errors that occur during logging
+            self.handleError(record)
 
 
 def main():
@@ -21,6 +53,9 @@ def main():
     timestamp = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     with open("logs/log-" + timestamp + ".mcap", "wb") as f, Writer(f) as writer:
         start_time = time.time_ns()
+        foxglove_handler = FoxgloveLoggingHandler(writer)
+        foxglove_handler.setLevel(logging.DEBUG)
+        config.logger.addHandler(foxglove_handler)
         setup_field(start_time, writer)
         while True:
             now = time.time_ns()
