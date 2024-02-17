0274e0f3e2124345755ebefc38555542a2081ecf
Benson Zhou <bensonzhou@yahoo.com>
2024-02-16 21:06:00 -0800

    perception manager ssh connection works

---
+++ util/nt_interface.py
@@ -0,0 +1,69 @@
+import math
+from typing import List
+
+import ntcore
+
+import util.config as config
+from util.config import version
+from util.vision_types import Pose, TagObservation
+
+
+class NTInterface:
+    # Initiate Connection
+    def __init__(self, ip: str) -> None:
+        ntcore.NetworkTableInstance.getDefault().setServer(ip)
+        ntcore.NetworkTableInstance.getDefault().startClient4("Perception")
+        self.output_table = ntcore.NetworkTableInstance.getDefault().getTable(
+            "/Perception/" + config.device_id)
+        self.observations0_pub = self.output_table.getDoubleArrayTopic("observations0").publish(
+            ntcore.PubSubOptions(periodic=0, sendAll=True, keepDuplicates=True))
+        self.observations1_pub = self.output_table.getDoubleArrayTopic("observations1").publish(
+            ntcore.PubSubOptions(periodic=0, sendAll=True, keepDuplicates=True))
+        self.errors_pub = self.output_table.getDoubleArrayTopic("errors").publish(
+            ntcore.PubSubOptions(periodic=0, sendAll=True, keepDuplicates=True))
+        self.tags_pub = self.output_table.getIntegerArrayTopic("tag_ids").publish(
+            ntcore.PubSubOptions(periodic=0, sendAll=True, keepDuplicates=True))
+        self.fps_pub = self.output_table.getDoubleTopic("fps").publish()
+        self.version_pub = self.output_table.getStringTopic("version").publish()
+        self.version_pub.set(version)
+
+        self.config_table = ntcore.NetworkTableInstance.getDefault().getTable("/Perception/config")
+        self.tagignore_sub = self.config_table.getIntegerArrayTopic("ignored_tags").subscribe(config.ig)
+
+    def publish_data(self, pose0: Pose, pose1: Pose, tags: List[TagObservation], timestamp: float) -> None:
+        errors = []
+        observation_data = []
+        if pose0 is not None:
+            wpi_pose0 = pose0.get_wpilib()
+            observation_data.append(wpi_pose0.translation().X())
+            observation_data.append(wpi_pose0.translation().Y())
+            observation_data.append(wpi_pose0.translation().Z())
+            observation_data.append(wpi_pose0.rotation().getQuaternion().W())
+            observation_data.append(wpi_pose0.rotation().getQuaternion().X())
+            observation_data.append(wpi_pose0.rotation().getQuaternion().Y())
+            observation_data.append(wpi_pose0.rotation().getQuaternion().Z())
+            errors.append(pose0.error)
+        try:
+            fps = 9 / (config.fps[-1] - config.fps[-10])
+        except:
+            fps = 0
+        observation_data_2 = []
+        if pose1 is not None:
+            wpi_pose1 = pose1.get_wpilib()
+            observation_data_2.append(wpi_pose1.translation().X())
+            observation_data_2.append(wpi_pose1.translation().Y())
+            observation_data_2.append(wpi_pose1.translation().Z())
+            observation_data_2.append(wpi_pose1.rotation().getQuaternion().W())
+            observation_data_2.append(wpi_pose1.rotation().getQuaternion().X())
+            observation_data_2.append(wpi_pose1.rotation().getQuaternion().Y())
+            observation_data_2.append(wpi_pose1.rotation().getQuaternion().Z())
+            errors.append(pose1.error)
+
+        self.observations0_pub.set(observation_data, math.floor(timestamp * 1000000))
+        self.observations1_pub.set(observation_data_2, math.floor(timestamp * 1000000))
+        self.errors_pub.set(errors, math.floor(timestamp * 1000000))
+        self.fps_pub.set(fps, math.floor(timestamp * 1000000))
+        self.tags_pub.set([tag.tag_id for tag in tags], math.floor(timestamp * 1000000))
+
+    def get_config(self):
+        pass
