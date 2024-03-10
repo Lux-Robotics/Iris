9b12d9f335fb9b000254fa59856df7bbe491fcde
Benson Zhou <bensonzhou@yahoo.com>
2024-03-09 18:08:54 -0800

    update calibration script

--- tools/mrcal-docker/scripts/mrcal_calibrate.sh
+++ tools/mrcal-docker/scripts/mrcal_calibrate.sh
@@ -9,17 +9,20 @@ fi
 VIDEO_FILE="$1"
 IMAGE_DIR="img"
 
+# Extract the base name of the video (without the path and extension)
+VIDEO_NAME=$(basename "$VIDEO_FILE" | sed 's/\.[^.]*$//')
+
 # Create image directory if it doesn't exist
 mkdir -p "$IMAGE_DIR"
 
 # Extract frames from the provided video file
-ffmpeg -i "$VIDEO_FILE" -vsync 0 "$IMAGE_DIR"/output_%04d.png
+ffmpeg -i "$VIDEO_FILE" -vsync 0 "$IMAGE_DIR"/"${VIDEO_NAME}_output_%04d.png"
 
 # Run mrgingham to find corners
-mrgingham --jobs $(nproc) --gridn 13 "$IMAGE_DIR"/"*.png" > corners.vnl
+mrgingham --jobs $(nproc) --gridn 13 "$IMAGE_DIR"/"${VIDEO_NAME}_*.png" > "${VIDEO_NAME}_corners.vnl"
 
 # Run camera calibration
-mrcal-calibrate-cameras --corners-cache corners.vnl --lensmodel LENSMODEL_OPENCV8 --focal 900 --object-spacing 0.012 --object-width-n 13 "$IMAGE_DIR"/"*.png"
+mrcal-calibrate-cameras --corners-cache "${VIDEO_NAME}_corners.vnl" --lensmodel LENSMODEL_OPENCV8 --focal 900 --object-spacing 0.012 --object-width-n 13 "$IMAGE_DIR"/"${VIDEO_NAME}_*.png"
 
 # Remove the images directory
 rm -r "$IMAGE_DIR"
