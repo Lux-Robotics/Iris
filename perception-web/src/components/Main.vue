5cf9f8fc9c326b0d2dbb62072e8fb7e15c0a48cc
Benson Zhou <bensonzhou@yahoo.com>
2024-08-11 11:14:15 -0700

    add web ui

---
+++ perception-web/src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="7">
+          <CameraStream></CameraStream>
+        </v-col>
+        <v-col cols="12" md="5">
+          <v-row>
+            <v-col cols="12">
+              <AprilTagOptions></AprilTagOptions>
+            </v-col>
+            <v-col cols="12">
+              <Graph></Graph>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
---
+++ perception-web/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Main> </Main>
+</template>
+
+<script setup>
+  //
+</script>
