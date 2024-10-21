b1e343e8fb5592cbac605b5532dc1d40cf36bcf2
Benson Zhou <benson@bensonzhou.com>
2024-10-21 15:40:00 -0700

    client cleanup

--- iris-web/src/plugins/vuetify.ts
+++ iris-web/src/plugins/vuetify.ts
@@ -20,19 +20,9 @@ export default createVuetify({
 				colors: {
 					primary: "#5A4FCF",
 					secondary: "#B1ADE8",
-					// secondary: '#817F9F',
-					// background: '#1C1B1F',
-					// surface: '#26252D',
-					// background: '#252433',
 					background: "#1C1C1D",
-					// 'on-background': '#E5E1E9',
-					// surface: '#312f40',
 					surface: "#282829",
-					// 'on-surface': '#E5E1E9',
 					error: "#FF5449",
-					// info: '#2196F3',
-					// success: '#4caf50',
-					// warning: '#fb8c00',
 				},
 			},
 			light: {
@@ -40,7 +30,7 @@ export default createVuetify({
 				colors: {
 					error: "#FF5449",
 					primary: "#5A4FCF",
-					secondary: "#5A4FcF",
+					secondary: "#5A4FCF",
 					background: "#F0F0F0",
 				},
 			},
