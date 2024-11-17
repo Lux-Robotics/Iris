6e3586cacd044d1e6258a8585a513864686757eb
Benson Zhou <benson@bensonzhou.com>
2024-11-17 00:20:20 -0800

    no delay when displaying uptime

--- iris-web/src/components/settings/SystemInfo.vue
+++ iris-web/src/components/settings/SystemInfo.vue
@@ -47,71 +47,71 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
-import { NetworkTablesTypeInfos } from "ntcore-ts-client";
-import { ntcore } from "@/nt-listener";
+  import { onMounted, ref } from 'vue'
+  import { NetworkTablesTypeInfos } from 'ntcore-ts-client'
+  import { ntcore } from '@/nt-listener'
 
-const hardwareInfo = ref("Unknown");
-const version = ref("Unknown");
-const commitHash = ref("Unknown");
-const uptime = ref("00:00");
+  const hardwareInfo = ref('Unknown')
+  const version = ref('Unknown')
+  const commitHash = ref('Unknown')
+  const uptime = ref('00:00')
 
-function formatTime(numSeconds: number) {
-	const days = Math.floor(numSeconds / 86400);
-	const hours = Math.floor((numSeconds % 86400) / 3600);
-	const minutes = Math.floor((numSeconds % 3600) / 60);
-	const seconds = numSeconds % 60;
-	let result = "";
-	if (days > 0) {
-		result += days + ":";
-	}
-	if (days > 0 || hours > 0) {
-		result += (hours < 10 ? "0" : "") + hours + ":";
-	}
-	result += (minutes < 10 ? "0" : "") + minutes + ":";
-	result += (seconds < 10 ? "0" : "") + seconds;
-	return result;
-}
+  function formatTime (numSeconds: number) {
+    const days = Math.floor(numSeconds / 86400)
+    const hours = Math.floor((numSeconds % 86400) / 3600)
+    const minutes = Math.floor((numSeconds % 3600) / 60)
+    const seconds = numSeconds % 60
+    let result = ''
+    if (days > 0) {
+      result += days + ':'
+    }
+    if (days > 0 || hours > 0) {
+      result += (hours < 10 ? '0' : '') + hours + ':'
+    }
+    result += (minutes < 10 ? '0' : '') + minutes + ':'
+    result += (seconds < 10 ? '0' : '') + seconds
+    return result
+  }
 
-onMounted(() => {
-	const hardwareInfoTopic = ntcore.createTopic<string>(
-		"hardwareInfo",
-		NetworkTablesTypeInfos.kString,
-	);
-	const versionTopic = ntcore.createTopic<string>(
-		"version",
-		NetworkTablesTypeInfos.kString,
-	);
-	const commitHashTopic = ntcore.createTopic<string>(
-		"gitCommitHash",
-		NetworkTablesTypeInfos.kString,
-	);
+  onMounted(() => {
+    const hardwareInfoTopic = ntcore.createTopic<string>(
+      'hardwareInfo',
+      NetworkTablesTypeInfos.kString,
+    )
+    const versionTopic = ntcore.createTopic<string>(
+      'version',
+      NetworkTablesTypeInfos.kString,
+    )
+    const commitHashTopic = ntcore.createTopic<string>(
+      'gitCommitHash',
+      NetworkTablesTypeInfos.kString,
+    )
+    const uptimeTopic = ntcore.createTopic<number>('uptime', NetworkTablesTypeInfos.kInteger)
 
-	hardwareInfo.value = hardwareInfoTopic.getValue() ?? "Unknown";
-	version.value = versionTopic.getValue() ?? "Unknown";
-	commitHash.value = commitHashTopic.getValue() ?? "Unknown";
+    hardwareInfo.value = hardwareInfoTopic.getValue() ?? 'Unknown'
+    uptime.value = formatTime(uptimeTopic.getValue() ?? 0)
+    version.value = versionTopic.getValue() ?? 'Unknown'
+    commitHash.value = commitHashTopic.getValue() ?? 'Unknown'
 
-	hardwareInfoTopic.subscribe((v) => {
-		if (v !== null) {
-			hardwareInfo.value = v;
-		}
-	});
-	versionTopic.subscribe((v) => {
-		if (v !== null) {
-			version.value = v;
-		}
-	});
-	commitHashTopic.subscribe((v) => {
-		if (v !== null) {
-			commitHash.value = v;
-		}
-	});
-	ntcore
-		.createTopic<number>("uptime", NetworkTablesTypeInfos.kInteger)
-		.subscribe((v) => {
-			if (v !== null) {
-				uptime.value = formatTime(v);
-			}
-		});
-});
+    hardwareInfoTopic.subscribe(v => {
+      if (v !== null) {
+        hardwareInfo.value = v
+      }
+    })
+    versionTopic.subscribe(v => {
+      if (v !== null) {
+        version.value = v
+      }
+    })
+    commitHashTopic.subscribe(v => {
+      if (v !== null) {
+        commitHash.value = v
+      }
+    })
+    uptimeTopic.subscribe(v => {
+      if (v !== null) {
+        uptime.value = formatTime(v)
+      }
+    })
+  })
 </script>
